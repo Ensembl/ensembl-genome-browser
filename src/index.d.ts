0268581fe706efdcb029ebb9e9081e256296dbda
Manoj <manojpandian@ebi.ac.uk>
2021-06-10 13:47:44 +0100

    Update package JSON

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+import EnsemblGenomeBrowser from './ensemblGenomeBrowser';
+
+import { OutgoingAction, OutgoingActionType, IncomingAction, IncomingActionType } from './action';
+
+export {
+    OutgoingAction, 
+    OutgoingActionType, 
+    IncomingAction, 
+    IncomingActionType
+}
+
+export default EnsemblGenomeBrowser;
